ef52211598f7b0e9e8fea2c6eb97ba0b567725f1
diuguide <everett.diuguid@gmail.com>
2021-06-04 10:22:49 -0400

    congifured spring boot

---
+++ src/main/java/com/springboot/Driver.java
@@ -0,0 +1,15 @@
+package com.springboot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class Driver {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Driver.class, args);
+    }
+
+
+
+}
