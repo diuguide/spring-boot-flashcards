3eb845bf00a6b9faf3592cb1afa5814a02df9985
diuguide <everett.diuguid@gmail.com>
2021-06-08 09:01:10 -0400

    added credentials class

---
+++ src/main/java/com/springboot/dtos/Credentials.java
@@ -0,0 +1,27 @@
+package com.springboot.dtos;
+
+import javax.validation.constraints.NotEmpty;
+
+public class Credentials {
+
+    @NotEmpty
+    private String username;
+    @NotEmpty
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
