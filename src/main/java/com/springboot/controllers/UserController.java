a0f8124aee9246b8f8350641ae25dd0abc37ff92
diuguide <everett.diuguid@gmail.com>
2021-06-08 09:20:52 -0400

    login route tested works

--- src/main/java/com/springboot/controllers/UserController.java
+++ src/main/java/com/springboot/controllers/UserController.java
@@ -1,16 +1,25 @@
 package com.springboot.controllers;
 
 
+import com.springboot.dtos.Credentials;
+import com.springboot.models.User;
 import com.springboot.repos.UserRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     private UserRepository userRepo;
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
 
     public UserController(UserRepository userRepo) {
         this.userRepo = userRepo;
@@ -19,6 +28,35 @@ public class UserController {
     @GetMapping("/")
     public String test() {
         return "This route is working";
+    }
+
+    @PostMapping(value="/addUser", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+    public User registerNewUser(@RequestBody @Valid User newUser) {
+        logger.info("USER REGISTERED invoked at {}", LocalDateTime.now());
+        return userRepo.save(newUser);
+    }
+
+    @PostMapping(value="/login", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+    public void loginUser(@RequestBody @Valid Credentials creds) {
+        logger.info("User logged in");
+        User user = userRepo.findUserByUsernameAndPassword(creds.getUsername(), creds.getPassword());
+
+        if(user != null) {
+            logger.info("Successfully Logged in {}", user);
+        } else {
+            logger.info("Login Failed, please check credentials and try again");
+        }
+
+    }
+
+    @DeleteMapping(value="/deleteUser")
+    public void deleteUser(@RequestBody int id) {
+        logger.info("DELETE USER {} invoked at {}", id, LocalDateTime.now());
+        try {
+            userRepo.deleteById(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/main/java/com/springboot/repos/UserRepository.java
+++ src/main/java/com/springboot/repos/UserRepository.java
@@ -4,4 +4,7 @@ import com.springboot.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository <User, Integer> {
+
+    User findUserByUsernameAndPassword(String username, String password);
+
 }
